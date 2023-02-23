21f3c69a56b05a57533e1c9a8ef5c179252731a6
tuhoang1412 <tuhoang.1412@gmail.com>
2023-02-22 20:31:43 -0800

    Change #1

--- Assets/RockScript.cs
+++ Assets/RockScript.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class RockScript : MonoBehaviour
 {
     public float moveSpeed;
+    private int leftLimit;
     // Start is called before the first frame update
+    // private Vector3 XU, XD;
+    private float offsetX;
     void Start()
     {
-        moveSpeed = 4f;
+        SetXOffset();
+        moveSpeed = 5f;
+        leftLimit = -14;
     }
+    void SetXOffset()
+    {
+        offsetX = Random.Range(0.4f, 0.7f);
+        Vector3 horizontalShift = new Vector3(offsetX, 0, 0);
+        if (Random.value > 0.5)
+        {
+            transform.GetChild(0).gameObject.transform.position += horizontalShift;
+            transform.GetChild(1).gameObject.transform.position -= horizontalShift;
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.transform.position -= horizontalShift;
+            transform.GetChild(1).gameObject.transform.position += horizontalShift;
+        }
 
+    }
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
-        if (transform.position.x < -10)
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         }
--- Assets/RockSpawner.cs
+++ Assets/RockSpawner.cs
@@ -6,27 +6,33 @@ public class RockSpawner : MonoBehaviour
 {
     public GameObject rockPrefab;
     public GameObject rockIcePrefab;
-    private float offset;
+    private float offsetY;
     // Start is called before the first frame update
     void Start()
     {
+
         Invoke("Spawn", 0.3f);
     }
-    float GetOffset(float lower, float upper)
+    Vector3 GetYOffset()
     {
-        offset = Random.Range(lower, upper);
-        offset = Random.Range(lower, upper);
-        return Random.value > 0.5 ? offset : -offset;
+        offsetY = Random.Range(0.5f, 1.8f);
+        if (Random.value > 0.5)
+        {
+            return new Vector3(transform.position.x, transform.position.y + offsetY, transform.position.z);
+        }
+        else
+        {
+            return new Vector3(transform.position.x, transform.position.y - offsetY, transform.position.z);
+        }
     }
+
     void Spawn()
     {
-        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + GetOffset(0.5f, 1.8f), transform.position.z);
-        GameObject rock = Random.value < 0.9 ? Instantiate<GameObject>(rockPrefab, spawnPosition, transform.rotation) : Instantiate<GameObject>(rockIcePrefab, spawnPosition, transform.rotation);
-        Vector3 shiftPosition = new Vector3(GetOffset(0.3f, 0.6f), 0, 0);
-        GameObject up = rock.transform.GetChild(0).gameObject;
-        up.transform.position += shiftPosition;
-        GameObject down = rock.transform.GetChild(1).gameObject;
-        down.transform.position -= shiftPosition;
+        // SetXOffset();
+        // Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + GetOffset(0.5f, 1.8f), transform.position.z);
+        GameObject rock = Random.value < 0.9 ? Instantiate<GameObject>(rockPrefab, GetYOffset(), transform.rotation) : Instantiate<GameObject>(rockIcePrefab, GetYOffset(), transform.rotation);
+        // Vector3 shiftPosition = new Vector3(GetOffset(0.3f, 0.6f), 0, 0);
+        // down.transform.position -= shiftPosition;
         Invoke("Spawn", Random.Range(1.4f, 1.9f));
     }
     // Update is called once per frame
